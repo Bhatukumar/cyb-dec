9c05eab3fd99ecc3baeae166abba33f8411fb8cb
deepammali <deepammali0@gmail.com>
2022-07-03 01:16:32 +0530

    Controller Implemented

--- controller.py
+++ controller.py
@@ -6,6 +6,7 @@ from modules import base32c as bs32
 from modules import base85 as bs85
 from modules import hexc as hex
 from modules import rot13c as rot13
+from modules import plaintext as pt
 
 
 def main():
@@ -43,20 +44,85 @@ def work(Text, From, To):
         if To == "Plaintext":
             return Text
         elif To == "Hex":
-            return hex.encode()
+            return pt.plaintextToHex(Text)
         elif To == "Base32":
-            return bs32.encode()
+            return pt.plaintextToBase32(Text)
         elif To == "Base64":
-            return bs64.encode()
+            return pt.plaintextToBase64(Text)
         elif To == "Base85":
-            return bs85.encode()
+            return pt.plaintextToBase85(Text)
         elif To == "ROT13":
-            return rot13.encode()
+            return pt.plaintextToRot13(Text)
+
     if From == "Hex":
         if To == "Hex":
             return Text
         elif To == "Plaintext":
-            return hex.decode(Text)
+            return hex.hexToPlaintext(Text)
+        elif To == "Base32":
+            return hex.hexToBase32(Text)
+        elif To == "Base64":
+            return hex.hexToBase64(Text)
+        elif To == "Base85":
+            return hex.hexToBase85(Text)
+        elif To == "ROT13":
+            return hex.hexToRot13(Text)
+
+    if From == "Base32":
+        if To == "Base32":
+            return Text
+        elif To == "Plaintext":
+            return bs32.base32ToPlaintext(Text)
+        elif To == "Base64":
+            return bs32.base32ToBase64(Text)
+        elif To == "Base85":
+            return bs32.base32ToBase85(Text)
+        elif To == "ROT13":
+            return bs32.base32ToRot13(Text)
+        elif To == "Hex":
+            return bs32.base32ToHex(Text)
+
+    if From == "Base64":
+        if To == "Base64":
+            return Text
+        elif To == "Plaintext":
+            return bs64.base64ToPlaintext(Text)
+        elif To == "Base32":
+            return bs64.base64ToBase32(Text)
+        elif To == "Base85":
+            return bs64.base64ToBase85(Text)
+        elif To == "Hex":
+            return bs64.base64ToHex(Text)
+        elif To == "ROT13":
+            return bs64.base64ToRot13(Text)
+
+    if From == "Base85":
+        if To == "Base85":
+            return Text
+        elif To == "Plaintext":
+            return bs85.base85ToPlaintext(Text)
+        elif To == "Hex":
+            return bs85.base85ToHex(Text)
+        elif To == "Base32":
+            return bs85.base85ToBase32(Text)
+        elif To == "Base64":
+            return bs85.base85ToBase64(Text)
+        elif To == "ROT13":
+            return bs85.base85ToRot13(Text)
+        
+    if From == "ROT13":
+        if To == "ROT13":
+            return Text
+        elif To == "Plaintext":
+            return rot13.rot13ToPlaintext(Text)
+        elif To == "Base32":
+            return rot13.rot13ToBase32(Text)
+        elif To == "Base64":
+            return rot13.rot13ToBase64(Text)
+        elif To == "Base85":
+            return rot13.rot13ToBase85(Text)
+        elif To == "Hex":
+            return rot13.rot13ToHex(Text)
     return False
 if __name__ == '__main__':
     main()
